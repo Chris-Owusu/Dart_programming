9e5721605035659a3baa9abe0416d06ff4378c2e
Chris-Owusu <christopher.owusu@amalitech.com>
2024-08-08 12:11:50 +0000

    variables

---
+++ variables.dart
@@ -0,0 +1,27 @@
+void main() {
+  // variables
+
+
+  // strings
+  String name = "Jong";
+  print("My name is " + name);
+
+  // Integers
+  var x = 23;
+  print(x);
+
+  // Dynamic variables
+  dynamic firstName = "Matthew";
+  print(firstName + ": This is a dynamic variable");
+
+
+  // const and final
+  const String fullName = "John Elder";   // used at compiled time
+  final age = 34;    // used at runtime. eg the time always change
+
+  print(fullName + " is " + age + " years old.");
+
+
+
+
+}
