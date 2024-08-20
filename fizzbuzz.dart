22daea438b9c35509e3962dfbbf7d43e344159fd
Chris-Owusu <christopher.owusu@amalitech.com>
2024-08-20 07:53:37 +0000

    another implementation of fizzbuzz in dart

--- fizzbuzz.dart
+++ fizzbuzz.dart
@@ -19,4 +19,21 @@ void main() {
     print("Number is not divisible by either 3 or 5");
   }
 
+
+  //--------------------------------------------------------------------------------------
+
+  int nums = 100;
+
+  for (int i = 0; i < nums; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+    print("$i is FizzBuzz");
+  } else if (i % 5 == 0) {
+    print("$i is Buzz");
+  } else if (i % 3 == 0) {
+    print("$i is Fizz");
+  } else {
+    print("Number is not divisible by either 3 or 5");
+  }
+  }
+
 }
