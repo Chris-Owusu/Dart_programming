4494eac51b6f811ae36fbc7099309f7fc76c311a
Chris-Owusu <christopher.owusu@amalitech.com>
2024-08-20 06:19:29 +0000

    logic dart

---
+++ logic.dart
@@ -0,0 +1,13 @@
+void main() {
+  // Logic - If, Else, If Else If
+
+  var num = 1;
+  if (num == 3) {
+    print("the number is $num");
+  } else if (num <= 3) {
+    print("the number is less than 3");
+  } else {
+    print("Number is probably greater that 3");
+  }
+
+}
